2982bcd34e03e92b6380b71ddaeb52b169da0f95
Kristian García <kristianpro498@gmail.com>
2024-11-19 11:55:08 +0100

    He metido todo en un bucle, ya funciona bien

--- src/calculadora.java
+++ src/calculadora.java
@@ -2,43 +2,48 @@ import java.util.Scanner;
 
 public class calculadora {
     public static void main(String[] args) {
+        int input = 1;
 
-        // MENU
-        System.out.println("""
-                ¿Que operación quiere realizar?
-                1) Suma
-                2) Resta
-                3) Multiplicación
-                4) División
-                """);
+        while (input != 5) {
+            System.out.println("Dime el primer número");
+            double num1 = new Scanner(System.in).nextInt();
 
-        int input = new Scanner(System.in).nextInt();
+            System.out.println("Dime el segundo número");
+            double num2 = new Scanner(System.in).nextInt();
 
-        System.out.println("Dime el primer número");
-        double num1 = new Scanner(System.in).nextInt();
+            // MENU
+            System.out.println("""
+                    ¿Que operación quiere realizar?
+                    1) Suma
+                    2) Resta
+                    3) Multiplicación
+                    4) División
+                    5) Salir
+                    """);
 
-        System.out.println("Dime el segundo número");
-        double num2 = new Scanner(System.in).nextInt();
+            input = new Scanner(System.in).nextInt();
 
-        double resultado = 1;
+            double resultado = 1;
 
-        switch (input){
-            case 1 ->{
-                resultado = num1 + num2;
+            switch (input) {
+                case 1 -> {
+                    resultado = num1 + num2;
+                }
+
+                case 2 -> {
+                    resultado = num1 - num2;
+                }
+
+                case 3 -> {
+                    resultado = num1 * num2;
+                }
+
+                case 4 -> {
+                    resultado = num1 / num2;
+                }
             }
-
-            case 2 ->{
-                resultado = num1 - num2;
-            }
-
-            case 3 ->{
-                resultado = num1 * num2;
-            }
-
-            case 4 ->{
-                resultado = num1 / num2;
-            }
+            System.out.println("El resultado de la operación es: " + resultado);
+            String esperar = new Scanner(System.in).nextLine();
         }
-        System.out.println("El resultado de la operación es: " + resultado);
     }
 }
