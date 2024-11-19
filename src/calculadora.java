f9ca1bc43e305bd2ca193abdb1c7f2fc3b8b0a6c
Kristian García <kristianpro498@gmail.com>
2024-11-19 11:05:51 +0100

    He añadido el MENU

--- src/calculadora.java
+++ src/calculadora.java
@@ -2,14 +2,26 @@ import java.util.Scanner;
 
 public class calculadora {
     public static void main(String[] args) {
+
+        // MENU
+        System.out.println("""
+                ¿Que operación quiere realizar?
+                1) Suma
+                2) Resta
+                3) Multiplicación
+                4) División
+                """);
+
+        int input = new Scanner(System.in).nextInt();
+
         System.out.println("Dime el primer numero");
         double num1 = new Scanner(System.in).nextInt();
 
         System.out.println("Dime el segundo numemro");
         double num2 = new Scanner(System.in).nextInt();
 
-        System.out.println("Has introducido los numeros " + num1 + " y " + num2);
-
         double resultado = 1;
+
+
     }
 }
